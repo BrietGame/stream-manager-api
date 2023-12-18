26946f63ab3106310d9d2ff0a5124e005bc7447b
Alexis BRIET <alexis.briet2003@gmail.com>
2023-12-18 18:35:38 +0100

    Init project

---
+++ src/test/java/com/streammanager/api/StreamManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streammanager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
